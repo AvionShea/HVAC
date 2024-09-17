29d053af0544e1a857474ea590ac404fb1c1b451
AvionShea <avionsheadesigns@gmail.com>
2024-09-17 15:47:06 -0400

    code changes

--- model/appointmentModels.js
+++ model/appointmentModels.js
@@ -1,4 +1,4 @@
-const mongoose = required("mongoose");
+const mongoose = require("mongoose");
 
 const appointmentSchema = new mongoose.Schema({
   name: { type: String, required: true},
--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(express.json());
 
 //app.use("/api", apiRoutes);
 
-mongoose.connect(ProcessingInstruction.env.DB_CONNECTION)
-.then(() => console.log("MongoDB connected"))
+mongoose.connect(process.env.DB_CONNECTION)
+.then(() => console.log("MongoDB connected successfully!"))
 .catch(err => console.log(err));
 
-const PORT = ProcessingInstruction.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
