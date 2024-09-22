43556158b360d0737e6e2122476b86f39388055c
AvionShea <avionsheadesigns@gmail.com>
2024-09-22 17:55:56 -0400

    finished basics of project - everything works as attended

--- routes/router.js
+++ routes/router.js
@@ -1,49 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const Appointment = require("../model/appointmentModels.js");
-const path = require("path");
-require("dotenv").config();
+const controller = require('../controllers/controllers');
 
-const {MailtrapTransport} = require("mailtrap");
-const Nodemailer = require("nodemailer");
+// Route to schedule an appointment
+router.post('/schedule_service', controller.scheduleService);
 
-const TOKEN = process.env.MAIL_TOKEN;
-
-const transport = Nodemailer.createTransport(
-  MailtrapTransport({
-    token: TOKEN,
-  })
-);
-
-const sender = {
-  address: "hello@demomailtrap.com",
-  name: "Heat Wave Warriors",
-};
-
-router.post("/schedule_service", async (req, res) => {
-  const {name, email, phone, date} = req.body
-
-  let scheduleEmailText = ` We've received your request to schedule an appointment for ${date}.
-  
-  We will give you a call at ${phone} to verify your service, date, and time.`
-
-  try {
-    const newAppointment = new Appointment({name, email, phone, date})
-    await newAppointment.save()
-
-    transport.sendMail({
-    from: sender,
-    to: email,
-    subject: "HVAC Appointment Request",
-    text: scheduleEmailText,
-    category: "Appointment Request",
-  })
-
-    res.sendFile(path.join(__dirname,"../views/bookSuccess.html"))
-  } catch (err) {
-    console.log(err)
-    res.sendFile(path.join(__dirname,"../views/bookError.html"))
-  };
-});
+// Route to handle request quote email
+router.post('/request_quote', controller.requestQuote);
 
 module.exports = router;
